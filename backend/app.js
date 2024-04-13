bb7c584c5164f5909926fb2fda5df583dbaacd1e
kubra <kubra.roshi@gmail.com>
2024-04-13 20:28:59 +0530

    added code to connect mongodb atlast url to our backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
+const connectDB = require("./config/db");
 
 dotenv.config();
 const app = express();
+connectDB();
 
 const base_url = process.env.BASE_URL;
 
@@ -14,20 +16,12 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  console.log(req.params.id);
-  const singleChat = chats.chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
-
-mongoose
-  .connect(base_url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB", err));
+// mongoose
+//   .connect(base_url, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("Error connecting to MongoDB", err));
 
 app.listen(5000, () => {
   console.log("App listening on port 5000");
